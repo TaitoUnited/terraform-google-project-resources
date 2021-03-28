cddfa719838c65684f62fd4674ee640de1d1113d
keskiju <jukka.keskiluopa@taitounited.fi>
2021-03-28 23:30:46 +0300

    chore: update versions

--- versions.tf
+++ versions.tf
@@ -15,13 +15,16 @@
  */
 
 terraform {
-  required_version = ">= 0.13"
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = "=3.61.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
+      version = "=3.61.0"
     }
   }
+
+  required_version = ">= 0.14"
 }
